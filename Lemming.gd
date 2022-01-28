cd6a4eeab2bd8e046e2c4574e62539ac77a78c39
Charlie <charliewagbo@gmail.com>
2022-01-28 22:26:27 +0000

    init

---
+++ Lemming.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+var velocity = Vector2.ZERO
+export var speed = 50
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _physics_process(delta):
+	var move_vector = Vector2.LEFT
+	var pos = get_viewport().get_mouse_position()
+	var dis = $CollisionShape2D.global_position.distance_to(pos)
+	var dir = $CollisionShape2D.global_position.direction_to(pos)
+	if(dis<150):
+		velocity = -dir*(speed/dis)
+	else:
+		velocity = Vector2.ZERO
+	move_and_collide(velocity)
