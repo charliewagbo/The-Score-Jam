69b0e2a52e6855868f97a7291f78c8169fc72791
Charlie <charliewagbo@gmail.com>
2022-01-28 23:12:38 +0000

    feat: lemming is now penguin

--- Lemming.gd
+++ Lemming.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 export var speed = 50
+export var triger_range = 200
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,13 +15,13 @@ func _ready():
 #func _process(delta):
 #	pass
 func _physics_process(delta):
-	var move_vector = Vector2.LEFT
 	var pos = get_viewport().get_mouse_position()
 	var dis = $CollisionShape2D.global_position.distance_to(pos)
 	var dir = $CollisionShape2D.global_position.direction_to(pos)
-	if(dis<150):
-		velocity = -dir*(speed/dis)
+	if(dis<triger_range):
+		velocity = -dir*(0.3+(speed/dis))
 		look_at(global_position-dir)
 	else:
 		velocity = Vector2.ZERO
+		look_at(pos)
 	move_and_collide(velocity)
